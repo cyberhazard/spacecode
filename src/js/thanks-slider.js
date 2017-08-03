91ac4ba70c3a25acc4c3c2f5cf238e163284937a
cyber <cyber>
2017-08-03 23:57:13 +0300

    slider

--- src/js/thanks-slider.js
+++ src/js/thanks-slider.js
@@ -4,5 +4,22 @@ export const slider = new Swiper('.reviews-slider', {
   pagination: '.reviews-pagination',
   slidesPerView: 3,
   paginationClickable: true,
-  spaceBetween: 30
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is <= 320px
+    740: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is <= 480px
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is <= 640px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    }
+  }
 });
