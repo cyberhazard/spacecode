d008c140907293c6d3d384106085106cf4f6b8fa
cyber <cyber>
2017-08-03 04:04:06 +0300

    add: callback window script

---
+++ src/js/callback.js
@@ -0,0 +1,26 @@
+export default () => {
+  const button  = document.querySelector('#header-button');
+  const form = document.querySelector('#callback-form');
+  const layout = document.querySelector('.callback');
+  const closeButton = document.querySelector('.callback__close');
+  if(!button || !form || !layout || !closeButton) return null
+
+  const layoutClose = () =>
+    (layout.style.display = '',
+    document.body.style.paddingRight = '',
+    document.body.style.overflow = '',
+    layout.removeEventListener('transitionend', layoutClose));
+
+  closeButton.onclick = () => {
+    layout.style.opacity = 0;
+    layout.addEventListener('transitionend', layoutClose);
+  };
+
+  button.onclick = () => {
+    layout.style.display = 'flex';
+    form.reset();
+    document.body.style.paddingRight = window.innerWidth - document.body.clientWidth + 'px';
+    document.body.style.overflow = 'hidden';
+    setTimeout(()=>layout.style.opacity = 1,0);
+  };
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import filter from './portfolioFilter';
 import { slider } from './thanks-slider';
 import { startGlow } from './headerStars';
 import { calc } from './calc';
+import callback from './callback'
 
 filter();
 startGlow();
 calc();
+callback()
