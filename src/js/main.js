efd662c80fa4d3e92e9fdf40cfcab1b6784c9640
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-08-10 04:48:59 +0700

    scroll to

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,26 @@ import { slider, lightBox } from './thanks-slider';
 import { startGlow } from './headerStars';
 import { calc } from './calc';
 import callback from './callback';
+import anim from 'animated-scroll-to';
 
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu__close');
 
 hamburger.onclick = () => mobileMenu.style.transform = 'translateX(0)'
-mobileMenuClose.onclick = () => mobileMenu.style.transform = ''
+mobileMenuClose.onclick = () => mobileMenu.style.transform = '';
+
+// Header Mouse animated scroll
+const mouse = document.querySelector('.header__mouse');
+const scrollToBlock = document.querySelector('.services');
+if(mouse && scrollToBlock){
+  mouse.onclick = () => {
+    const bodyScroll = document.body.scrollTop;
+    const scroll = scrollToBlock.getBoundingClientRect().top + bodyScroll;
+    console.log(scroll)
+    anim(scroll)
+  }
+}
 
 filter();
 startGlow();
